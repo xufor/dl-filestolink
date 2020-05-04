0ddd9535a08b4139dfc252f452b62f5b27e4c238
Akash Sharma <xufor.developer.official@gmail.com>
2020-05-04 17:59:12 +0530

    Removed exit warning

--- script.py
+++ script.py
@@ -41,5 +41,3 @@ if len(links) > 0:
                 if(position != -1 and response[index+2].strip() != "<div id='vidiv'>"):
                     print(r'http://www.filestolink.gq' + response[index+2].strip()[9:-2])
                     break
-
-input("Press any key to exit.")
